1866418c740556cad96bda5d83899a0de104c673
Anton Dolinsky <anton.dolinsky@lemon42.com>
2017-04-01 16:28:22 -0700

    More

--- content/objectbrowser/main.js
+++ content/objectbrowser/main.js
@@ -14,6 +14,6 @@ var main = function() {
 	getfiles(paths.templates).then(function(templates) {
 		var ob = objectbrowser(templates);
 		ob.set(testval);
-		document.body.appendChild(ob.getRoot());
+		document.body.appendChild(ob.dom());
 	});
 };
