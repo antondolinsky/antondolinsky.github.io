afd0017e07108465c3b6dd2e51dea95da01ea61a
Anton Dolinsky <anton.dolinsky@lemon42.com>
2017-04-01 18:30:02 -0700

    More

--- content/objectbrowser/src/js/objectbrowser.js
+++ content/objectbrowser/src/js/objectbrowser.js
@@ -1,3 +1,41 @@
+var objbrowseritem = function(templates, key, value, pathsofar, expanded) {
+	var path = pathsofar.slice();
+	path.push(key);
+	var item = domfromstr(templates.item)[0];
+	var ec = domqsaarray('[data-expandcollapse]', item)[0];
+	var ch = domqsaarray('[data-children]', item)[0];
+	domfillfields(item, 'data-field', {
+		key: key,
+		valuesummary: value.toString()
+	});
+	item.setAttribute('data-path', path.join('.'));
+	var isexpandable = (value instanceof Object);
+	var haschildren = (isexpandable && (Object.keys(value).length > 0));
+	item.setAttribute('data-isexpandable', isexpandable);
+	item.setAttribute('data-haschildren', haschildren);
+	var expandcollapse = function(mode) {
+		if (mode) {
+			ch.style.display = 'none';
+		} else {
+			ch.style.display = 'initial';
+			domempty(ch);
+			objeach(value, function(key, value) {
+				var item = objbrowseritem(templates, key, value, path);
+				ch.appendChild(item);
+			});
+		}
+		ec.setAttribute('data-expanded', (! mode));
+	};
+	expandcollapse(expanded);
+	if (ec && isexpandable) {
+		ec.setAttribute('data-expanded', 'false');
+		ec.addEventListener('click', function(e) {
+			expandcollapse(ec.getAttribute('data-expanded') === 'true');
+		});
+	}
+	return item;
+};
+
 var objectbrowser = function(templates) {
 	var el = domfromstr(templates.objectbrowser)[0];
 	var items = domqsaarray('[data-items]', el)[0];
@@ -8,15 +46,6 @@ var objectbrowser = function(templates) {
 		},
 		set: function(_root) {
 			root = _root;
-			domempty(items);
-			objeach(root, function(key, value) {
-				var item = domfromstr(templates.item)[0];
-				domfillfields(item, 'data-field', {
-					key: key,
-					valuesummary: value.toString()
-				});
-				items.appendChild(item);
-			});
 		}
 	};
 	return objectbrowser;
