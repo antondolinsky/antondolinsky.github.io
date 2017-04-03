80dca46055166f726a1fe10457a6e49bb929663c
Anton Dolinsky <anton.dolinsky@lemon42.com>
2017-04-02 19:14:52 -0700

    More

--- content/objectbrowser/src/js/objectbrowser.js
+++ content/objectbrowser/src/js/objectbrowser.js
@@ -1,4 +1,4 @@
-var objbrowseritem = function(templates, key, value, isexpanded) {
+var objbrowseritem = function(templates, key, value, isexpanded, eventscontroller) {
 	var type = typeof(value);
 	var specifictype = ((value instanceof Array) ? 'array' : type);
 	var isobjectorfunction = ((type === 'object') || (type === 'function'));
@@ -8,6 +8,7 @@ var objbrowseritem = function(templates, key, value, isexpanded) {
 	var $item = domfromstr(templates.item)[0];
 	var $ec = domqsaarray('[data-expandcollapse]', $item)[0];
 	var $ch = domqsaarray('[data-children]', $item)[0];
+	var $rest = domqsaarray('[data-rest]', $item)[0];
 	domfillfields($item, 'data-field', {
 		key: key,
 		valuesummary: valuesummary
@@ -24,18 +25,31 @@ var objbrowseritem = function(templates, key, value, isexpanded) {
 		if (mode) {
 			if ($item.getAttribute('data-hasbeenexpanded') === 'false') {
 				objeach(value, function(key, value) {
-					var $item = objbrowseritem(templates, key, value, false);
+					var $item = objbrowseritem(templates, key, value, false, eventscontroller);
 					$ch.appendChild($item);
 				});
-				$item.setAttribute('data-hasbeenexpanded', true);
+				domsetattributes($item, {
+					'data-hasbeenexpanded': true
+				});
 			}
 		}
-		$item.setAttribute('data-isexpanded', mode);
+		domsetattributes($item, {
+			'data-isexpanded': mode
+		});
 	};
+	$rest.addEventListener('click', function(e) {
+		var mode = ($item.getAttribute('data-selected') === 'false');
+		domsetattributes($item, {
+			'data-selected': mode
+		});
+		eventscontroller('setselected', [value, $item, mode]);
+	});
 	if (isoutlinked) {
 		expandcollapse(isexpanded);
 		$ec.addEventListener('click', function(e) {
-			expandcollapse($item.getAttribute('data-isexpanded') !== 'true');
+			var mode = ($item.getAttribute('data-isexpanded') !== 'true');
+			expandcollapse(mode);
+			eventscontroller('setexpanded', [value, $item, mode]);
 		});
 	}
 	return $item;
@@ -46,7 +60,32 @@ var objectbrowser = function(templates) {
 	var $items = domqsaarray('[data-items]', $element)[0];
 	var root;
 	var history = [];
+	var listeners = {
+		setexpanded: [
+			function(value, $item, mode) {
+
+			}
+		],
+		setselected: [
+			function(value, $item, mode) {
+
+			}
+		]
+	};
+	var eventscontroller = function(type, args) {
+		if (listeners.hasOwnProperty(type)) {
+			listeners.type.forEach(function(cb) {
+				cb.apply(null, args);
+			});
+		}
+	};
 	var objectbrowser = {
+		listen: function(type, cb) {
+			if (! listeners.hasOwnProperty(type)) {
+				listeners[type] = [];
+			}
+			listeners[type].push(cb);
+		},
 		dom: function() {
 			return $element;
 		},
@@ -57,7 +96,7 @@ var objectbrowser = function(templates) {
 		set: function(_root) {
 			root = _root;
 			history.push(root);
-			var $rootitem = objbrowseritem(templates, null, root, true);
+			var $rootitem = objbrowseritem(templates, null, root, true, eventscontroller);
 			domempty($items);
 			$items.appendChild(domqsaarray('[data-children]', $rootitem)[0]);
 		}
