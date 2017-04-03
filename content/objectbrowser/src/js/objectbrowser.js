3ccb158831e087817ba51a080a9856cd5a70c5b0
Anton Dolinsky <anton.dolinsky@lemon42.com>
2017-04-02 19:47:27 -0700

    More

--- content/objectbrowser/src/js/objectbrowser.js
+++ content/objectbrowser/src/js/objectbrowser.js
@@ -6,48 +6,51 @@ var objbrowseritem = function(templates, key, value, isexpanded, eventscontrolle
 	var valuesummary = ((type === 'object') ? null :
 		((type === 'function') ? 'function' : value.toString()));
 	var $item = domfromstr(templates.item)[0];
-	var $ec = domqsaarray('[data-expandcollapse]', $item)[0];
-	var $ch = domqsaarray('[data-children]', $item)[0];
-	var $rest = domqsaarray('[data-rest]', $item)[0];
+	var $ch = domqsaarray('[data-eltype-children]', $item)[0];
+	var triggers = domqsaarray('[data-trigger]', $item).reduce(function(sofar, curr) {
+		var key = curr.getAttribute('data-trigger');
+		sofar[key] = curr;
+		return sofar;
+	}, {});
 	domfillfields($item, 'data-field', {
 		key: key,
 		valuesummary: valuesummary
 	});
 	domsetattributes($item, {
-		'data-key': key,
-		'data-type': type,
-		'data-specifictype': specifictype,
-		'data-isoutlinked': isoutlinked,
-		'data-hasbeenexpanded': false,
-		'data-selected': false
+		'data-prop-key': key,
+		'data-prop-type': type,
+		'data-prop-specifictype': specifictype,
+		'data-prop-isoutlinked': isoutlinked,
+		'data-prop-hasbeenexpanded': false,
+		'data-prop-selected': false
 	});
 	var expandcollapse = function(mode) {
 		if (mode) {
-			if ($item.getAttribute('data-hasbeenexpanded') === 'false') {
+			if ($item.getAttribute('data-prop-hasbeenexpanded') === 'false') {
 				objeach(value, function(key, value) {
 					var $item = objbrowseritem(templates, key, value, false, eventscontroller);
 					$ch.appendChild($item);
 				});
 				domsetattributes($item, {
-					'data-hasbeenexpanded': true
+					'data-prop-hasbeenexpanded': true
 				});
 			}
 		}
 		domsetattributes($item, {
-			'data-isexpanded': mode
+			'data-prop-isexpanded': mode
 		});
 	};
-	$rest.addEventListener('click', function(e) {
-		var mode = ($item.getAttribute('data-selected') === 'false');
+	triggers.$select.addEventListener('click', function(e) {
+		var mode = ($item.getAttribute('data-prop-selected') === 'false');
 		domsetattributes($item, {
-			'data-selected': mode
+			'data-prop-selected': mode
 		});
 		eventscontroller('setselected', [value, $item, mode]);
 	});
 	if (isoutlinked) {
 		expandcollapse(isexpanded);
-		$ec.addEventListener('click', function(e) {
-			var mode = ($item.getAttribute('data-isexpanded') !== 'true');
+		triggers.$expand.addEventListener('click', function(e) {
+			var mode = ($item.getAttribute('data-prop-isexpanded') !== 'true');
 			expandcollapse(mode);
 			eventscontroller('setexpanded', [value, $item, mode]);
 		});
@@ -57,7 +60,7 @@ var objbrowseritem = function(templates, key, value, isexpanded, eventscontrolle
 
 var objectbrowser = function(templates) {
 	var $element = domfromstr(templates.objectbrowser)[0];
-	var $items = domqsaarray('[data-items]', $element)[0];
+	var $items = domqsaarray('[data-eltype-items]', $element)[0];
 	var root;
 	var history = [];
 	var listeners = {
@@ -98,7 +101,7 @@ var objectbrowser = function(templates) {
 			history.push(root);
 			var $rootitem = objbrowseritem(templates, null, root, true, eventscontroller);
 			domempty($items);
-			$items.appendChild(domqsaarray('[data-children]', $rootitem)[0]);
+			$items.appendChild(domqsaarray('[data-eltype-children]', $rootitem)[0]);
 		}
 	};
 	return objectbrowser;
