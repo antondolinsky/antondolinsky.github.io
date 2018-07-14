c6e343a15699b9d7a2770c191cbd02016540f659
Anton Dolinsky <anton.dolinsky@lemon42.com>
2018-07-14 07:58:11 -0700

    more

--- content/test/test-folder/index.js
+++ content/test/test-folder/index.js
@@ -1,5 +1,22 @@
 const onload = function() {
-	alert('in onload');
+
+	const targetNode = document.querySelector('div');
+	const observerOptions = {
+	  childList: true,
+	  attributes: true,
+	  subtree: true
+	};
+
+	const observer = new MutationObserver((...args) => {
+		console.log(...args);
+	});
+
+	observer.observe(targetNode, observerOptions);
+
+	targetNode.setAttribute('testProp', 'testVal');
+
+	observer.disconnect();
+
 };
 
 export { onload };
