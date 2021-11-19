1aa8e94023613a45b556b83c3fa4bab262932499
Anton Glukhareff (also known as Anton Dolinsky) <anton.dolinsky@gmail.com>
2021-11-19 10:13:34 -0800

    Query

--- content/new/index.js
+++ content/new/index.js
@@ -1,12 +1,45 @@
 window.addEventListener('DOMContentLoaded', () => {
-  const CANV_SIZE = 600;
+  const defaults = {
+    canvSize: 600,
+    depth: 32,
+    width: 16,
+    tCount: 2,
+    step: 0.001,
+    ts: null,
+    f: null
+  };
 
-  const depth = 32;
-  const width = 16;
+  const options = { ...defaults };
 
-  const tCount = 2;
+  const params = new URLSearchParams(window.location.search);
 
-  const f = new Float32Array(depth * width * (width + 1)).map(() => Math.random() * 2 - 1);
+  ['canvSize', 'depth', 'width', 'tCount', 'step'].forEach((name) => {
+    if (params.has(name)) {
+      options[name] = Number(params.get(name));
+    }
+  });
+  if (params.has('ts')) {
+    const paramStr = params.get('ts');
+    options.ts = paramStr.substring(1, paramStr.length - 1).split(',').map((item) => Number(item));
+  } else {
+    options.ts = new Array(options.tCount).fill(true).map(() => Math.random() * 2 - 1);
+  }
+  if (params.has('f')) {
+    const paramStr = params.get('f');
+    options.f = new Float32Array(paramStr.substring(1, paramStr.length - 1).split(',').map((item) => Number(item)));
+  } else {
+    options.f = new Float32Array(options.depth * options.width * (options.width + 1)).map(() => Math.random() * 2 - 1);
+  }
+
+  const { canvSize, depth, width, tCount, step, ts, f } = options;
+
+  const setParams = new URLSearchParams();
+  ['canvSize', 'depth', 'width', 'tCount', 'step'].forEach((name) => {
+    setParams.append(name, options[name]);
+  });
+  setParams.append('ts', `[${options.ts.map((t) => t.toString()).join(',')}]`);
+  setParams.append('f', `[${options.f.map((v) => v.toString()).join(',')}]`);
+  const queryStr = setParams.toString();
 
   const k = new Function('ts', `
 let depth = ${depth};
@@ -27,20 +60,18 @@ ${new Array(width).fill(true).map((e, index) => `i${index} = o${index};`).join('
 this.color((o0 + 1) / 2, (o1 + 1) / 2, (o2 + 1) / 2, 1);
   `);
 
-  console.log({ depth, width, f, k });
+  console.log({ options, queryStr, k });
 
   const gpu = new GPU();
 
   const render = gpu.createKernel(k)
-    .setOutput([CANV_SIZE, CANV_SIZE])
+    .setOutput([canvSize, canvSize])
     .setGraphical(true);
 
-  const genTFunc = () => {
-    const STEP = 0.001;
-    let t = Math.random() * 2 - 1;
+  const genTFunc = (t) => {
     return () => {
       const initialT = t;
-      t += STEP;
+      t += step;
       if (t >= 1) {
         t -= 2;
       }
@@ -57,6 +88,6 @@ this.color((o0 + 1) / 2, (o1 + 1) / 2, (o2 + 1) / 2, 1);
     window.requestAnimationFrame(() => go(tFuncs));
   };
 
-  const tFuncs = new Array(tCount).fill(true).map(() => genTFunc());
+  const tFuncs = new Array(tCount).fill(true).map((e, tIndex) => genTFunc(ts[tIndex]));
   window.requestAnimationFrame(() => go(tFuncs));
 });
